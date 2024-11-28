72aee4e9fb621f5bb9f80a6ac47c406a2f99fd76
Jeison ortiz <ojeison21@gmail.com>
2024-11-27 23:21:11 -0500

    api de jsonplaceholder

--- Profesor_Henry/27_Noviembre/script.js
+++ Profesor_Henry/27_Noviembre/script.js
@@ -1,17 +1,17 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json =>{
-        console.log(json)
-        var a = document.querySelector("body > main > section > section.section1 > article")
-        a.innerHTML = json.userId;
-
-        var b = document.querySelector("body > main > section > section.section2 > article")
-        b.innerHTML = json.id;
-
-        var c = document.querySelector("body > main > section > section:nth-child(3) > article")
-        c.innerHTML = json.title;
-
-        var d = document.querySelector("body > main > section > section:nth-child(4) > article")
-        d.innerHTML = json.completed;
-        })
-
+async function mostrarInformacion(params) {
+  const id = document.getElementById('idInput').value;
+  try{
+    const response = await fetch (`https://jsonplaceholder.typicode.com/todos/${id}`);
+    if (!response.ok){
+      throw new Error('Error al obtener la información');
+    }
+    const data = await response.json();
+    document.getElementById('idArticle').innerText = data.id;
+    document.getElementById('nombreArticle').innerText = data.userId;
+    document.getElementById('apellidoArticle').innerText = data.title;
+    document.getElementById('edadArticle').innerText = data.completed;
+  }catch(error){
+    alert(error.message);
+  }
+  
+}
