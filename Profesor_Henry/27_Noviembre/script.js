607cc00558142f573e494a416e7556e0f29413f6
Jeison ortiz <ojeison21@gmail.com>
2024-11-28 00:20:33 -0500

    listo

--- Profesor_Henry/27_Noviembre/script.js
+++ Profesor_Henry/27_Noviembre/script.js
@@ -1,17 +1,16 @@
-async function mostrarInformacion(params) {
+async function mostrarInformacion() {
   const id = document.getElementById('idInput').value;
-  try{
-    const response = await fetch (`https://jsonplaceholder.typicode.com/todos/${id}`);
-    if (!response.ok){
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    if (!response.ok) {
       throw new Error('Error al obtener la información');
     }
     const data = await response.json();
-    document.getElementById('idArticle').innerText = data.id;
-    document.getElementById('nombreArticle').innerText = data.userId;
-    document.getElementById('apellidoArticle').innerText = data.title;
-    document.getElementById('edadArticle').innerText = data.completed;
-  }catch(error){
+    document.getElementById("UsedId").innerText = data.userId;
+    document.getElementById("ID").innerText = data.id;
+    document.getElementById("Title").innerText = data.title;
+    document.getElementById("Completed").innerText = data.completed;
+  } catch (error) {
     alert(error.message);
   }
-  
 }
